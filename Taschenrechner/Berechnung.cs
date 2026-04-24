2ee78ab661266f5aaeeda0b71e9726d369eded83
roman <roman@DESKTOP-1O1EAEQ>
2026-04-24 12:34:19 +0200

    Projektdateien hinzufügen.

---
+++ Taschenrechner/Berechnung.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace Taschenrechner {
+    internal class Berechnung {
+        public static double rechnen(double z1, double z2, char op) {
+            return op switch
+            {
+                '+' => Addition(z1, z2),
+                '-' => Substraktion(z1, z2),
+                '*' => Multiplikation(z1, z2),
+                '/' => Division(z1, z2),
+                _ => throw new ArgumentOutOfRangeException(nameof(op), op, "Ugültiger OPerator")
+            };
+        } 
+        private static double Addition(double z1, double z2) {
+            return z1 + z2;
+        }
+        private static double Substraktion(double z1, double z2) {
+            return z1 - z2;
+        }
+        private static double Multiplikation(double z1, double z2) {
+            return z1 * z2;
+        }
+        public static double Division(double z1, double z2) {
+            if (z2 != 0) {
+                return z1 / z2;
+            } else {
+                throw new DivideByZeroException();
+            }
+            
+        }
+    }
+}
---
+++ Taschenrechner/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Taschenrechner {
+    internal class Program {
+        static void Main(string[] args) {
+
+            char op;
+            string input;
+
+            Console.Write("Zahl 1: ");
+            input = Console.ReadLine()!.Replace(',', '.');
+            if (double.TryParse(input, out double z1)) {
+            } else {
+                throw new ArgumentException("Ungültige Eingabe!");
+            }
+
+            Console.Write("Zahl 2: ");
+            input = Console.ReadLine()!.Replace(',', '.');
+            if (double.TryParse(input, out double z2)) {
+            } else {
+                throw new ArgumentException("Ungültige Eingabe");
+            }
+
+            Console.Write("Operator: ");
+            op = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            Console.WriteLine($"{z1} {op} {z2} = {Berechnung.rechnen(z1, z2, op)}");
+        }
+    }
+}
